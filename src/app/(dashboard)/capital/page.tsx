eea6162b76cad22f382893d854d4b5d00f249e8e
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:54:35 +0100

    feat: populate capital page with dummy text

---
+++ src/app/(dashboard)/capital/page.tsx
@@ -0,0 +1,14 @@
+interface CapitalPageProps {
+  
+}
+ 
+const CapitalPage: React.FC<CapitalPageProps> = () => {
+  return ( 
+    <div>
+      <h1>Capital Page</h1>
+      <p>This is the capital page</p>
+    </div>
+  );
+}
+ 
+export default CapitalPage;
