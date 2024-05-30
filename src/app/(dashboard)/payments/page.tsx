ab8a5b4917e4932873ea066adb93dbdc05dc2445
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:43:35 +0100

    feat: add payments page

---
+++ src/app/(dashboard)/payments/page.tsx
@@ -0,0 +1,14 @@
+interface PaymentsPageProps {
+  
+}
+ 
+const PaymentsPage: React.FC<PaymentsPageProps> = () => {
+  return ( 
+    <div>
+      <h1>Payments Page</h1>
+      <p>This is the payments page</p>
+    </div>
+  );
+}
+ 
+export default PaymentsPage;
