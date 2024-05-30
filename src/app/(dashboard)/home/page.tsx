a03ab04e26ee0bfc0035f6ab0990e6a72a0be63b
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:37:57 +0100

    feat: add home page

---
+++ src/app/(dashboard)/home/page.tsx
@@ -0,0 +1,16 @@
+import { Input } from "@/components/ui/input";
+
+interface HomePageProps {
+  
+}
+ 
+const HomePage: React.FC<HomePageProps> = () => {
+  return ( 
+    <div>
+      <h1>Home Page</h1>
+      <p>This is the home page</p>
+    </div>
+  );
+}
+ 
+export default HomePage;
