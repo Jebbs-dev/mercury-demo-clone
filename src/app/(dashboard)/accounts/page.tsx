0a1ade6a9439b7040839d37e746762919825c836
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:54:23 +0100

    feat: populate accounts page with dummy text

---
+++ src/app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,14 @@
+interface AccountsPageProps {
+  
+}
+ 
+const AccountsPage: React.FC<AccountsPageProps> = () => {
+  return ( 
+    <div>
+      <h1>Accounts Page</h1>
+      <p>This is the accounts page</p>
+    </div>
+  );
+}
+ 
+export default AccountsPage;
