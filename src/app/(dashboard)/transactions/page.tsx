b5e4deb94565fa96669e7fc094b21c6848faa406
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:43:47 +0100

    feat: add transactions page

---
+++ src/app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,14 @@
+interface TransactionsPageProps {
+  
+}
+ 
+const TransactionsPage: React.FC<TransactionsPageProps> = () => {
+  return ( 
+    <div>
+      <h1>Transactions Page</h1>
+      <p>This is the ransactions page</p>
+    </div>
+  );
+}
+ 
+export default TransactionsPage;
