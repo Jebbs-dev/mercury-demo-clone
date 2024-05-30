0f342efdb63249cbfc3ad9aba5d7ede7a5d8c836
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:54:04 +0100

    feat: populate accounting page with dummy text

---
+++ src/app/(dashboard)/accounting/page.tsx
@@ -0,0 +1,14 @@
+interface AccountingPageProps {
+  
+}
+ 
+const AccountingPage: React.FC<AccountingPageProps> = () => {
+  return ( 
+    <div>
+      <h1>Accounting Page</h1>
+      <p>This is the accounting page</p>
+    </div>
+  );
+}
+ 
+export default AccountingPage;
