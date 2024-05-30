3305da97913dbf1c0ddfee72ed74440042abf8fd
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:54:44 +0100

    feat: populate cards page with dummy text

---
+++ src/app/(dashboard)/cards/page.tsx
@@ -0,0 +1,14 @@
+interface CardsPageProps {
+  
+}
+ 
+const CardsPage: React.FC<CardsPageProps> = () => {
+  return ( 
+    <div>
+      <h1>Cards Page</h1>
+      <p>This is the cards page</p>
+    </div>
+  );
+}
+ 
+export default CardsPage;
