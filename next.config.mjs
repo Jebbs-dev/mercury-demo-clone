e20721d09c8f6a5b59209f16c62b690a398cd1d6
Jebbs-dev <ojebiyifulness@gmail.com>
2024-05-30 08:52:55 +0100

    config: add remotePattern configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-development.mercury.com',
+        port: '',
+      }
+    ],
+  },
+};
 
 export default nextConfig;
